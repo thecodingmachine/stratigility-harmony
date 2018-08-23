e06c549f0d02aa0e85b15d74181bbe87970838ca
David Négrier <d.negrier@thecodingmachine.com>
2018-08-23 15:02:40 +0200

    Migrating to Stratigility 3

--- src/StratigilityServiceProvider.php
+++ src/StratigilityServiceProvider.php
@@ -2,60 +2,78 @@
 
 namespace TheCodingMachine;
 
-use Interop\Container\ContainerInterface;
-use Interop\Container\ServiceProviderInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use TheCodingMachine\Funky\Annotations\Factory;
+use TheCodingMachine\Funky\Annotations\Tag;
+use TheCodingMachine\Funky\ServiceProvider;
 use Zend\Diactoros\Response;
-use Zend\Diactoros\Server;
+use Zend\Diactoros\ServerRequest;
+use Zend\Diactoros\ServerRequestFactory;
+use Zend\HttpHandlerRunner\Emitter\EmitterInterface;
+use Zend\HttpHandlerRunner\Emitter\SapiStreamEmitter;
+use Zend\HttpHandlerRunner\RequestHandlerRunner;
+use Zend\Stratigility\Middleware\ErrorResponseGenerator;
 use Zend\Stratigility\Middleware\NotFoundHandler;
 use Zend\Stratigility\MiddlewarePipe;
 
-class StratigilityServiceProvider implements ServiceProviderInterface
+class StratigilityServiceProvider extends ServiceProvider
 {
-    public function getFactories()
+    /**
+     * @Factory()
+     */
+    public static function createRequestHandlerRunner(RequestHandlerInterface $handler, EmitterInterface $emitter, ContainerInterface $container): RequestHandlerRunner
     {
-        return [
-            Server::class => [self::class, 'createServer'],
-            MiddlewarePipe::class => [self::class, 'createMiddlewarePipe'],
-        ];
-    }
-
-    public function getExtensions()
-    {
-        return [
-            MiddlewareListServiceProvider::MIDDLEWARES_QUEUE => [self::class, 'registerPageNotFoundMiddleware']
-        ];
-    }
-
-    public static function createServer(ContainerInterface $container) : Server
-    {
-        $server = Server::createServer($container->get(MiddlewarePipe::class), $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES);
-
-        return $server;
+        return new RequestHandlerRunner($handler, $emitter, $container->get('serverRequestFactory'), $container->get('serverRequestErrorResponseGenerator'));
     }
 
     /**
-     * This factory creates a Stratigility MiddlewarePipe.
-     * It also adds to the pipe any middlewares in the 'middlewaresQueue' service.
-     *
-     * @param ContainerInterface $container
-     * @return MiddlewarePipe
+     * @Factory(aliases={EmitterInterface::class})
      */
-    public static function createMiddlewarePipe(ContainerInterface $container) : MiddlewarePipe
+    public static function createSapiStreamEmitter(): SapiStreamEmitter
     {
-        $app = new MiddlewarePipe();
-        $app->setResponsePrototype(new Response());
-        $middlewaresQueue = $container->get(MiddlewareListServiceProvider::MIDDLEWARES_QUEUE);
-
-        /* @var $middlewaresQueue \SplPriorityQueue */
-        foreach ($middlewaresQueue as $middleware) {
-            $app->pipe($middleware);
-        }
-
-        return $app;
+        return new SapiStreamEmitter();
     }
 
-    public static function registerPageNotFoundMiddleware(ContainerInterface $container, \SplPriorityQueue $queue): \SplPriorityQueue
+    /**
+     * @Factory(aliases={RequestHandlerInterface::class})
+     */
+    public static function createRequestHandler(ContainerInterface $container): MiddlewarePipe
     {
-        $queue->insert(new NotFoundHandler(new Response()), MiddlewareOrder::PAGE_NOT_FOUND_LATE);
+        $queue = clone $container->get(MiddlewareListServiceProvider::MIDDLEWARES_QUEUE);
+        $middlewarePipe = new MiddlewarePipe();
+        foreach ($queue as $middleware) {
+            $middlewarePipe->pipe($middleware);
+        }
+        return $middlewarePipe;
+    }
+
+    /**
+     * @Factory(name="serverRequestFactory")
+     */
+    public static function createServerRequestFactory(): callable
+    {
+        return [ServerRequestFactory::class, 'fromGlobals'];
+    }
+
+    /**
+     * @Factory(name="serverRequestErrorResponseGenerator")
+     */
+    public static function createServerRequestErrorResponseGenerator(): callable
+    {
+        return function (\Throwable $e) {
+            $generator = new ErrorResponseGenerator();
+            return $generator($e, new ServerRequest(), new Response());
+        };
+    }
+
+    /**
+     * @Factory(tags={@Tag(name=MiddlewareListServiceProvider::MIDDLEWARES_QUEUE, priority=MiddlewareOrder::PAGE_NOT_FOUND_LATE)})
+     */
+    public static function registerPageNotFoundMiddleware(): NotFoundHandler
+    {
+        return new NotFoundHandler(function() {
+            return new Response();
+        });
     }
 }
